42a6f72baa2ef4257e73d10c09f9be74ab4d4df7
Atharva1214 <atharva.lende@eedeecreations.com>
2024-03-12 15:37:49 +0530

    G

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {gallery1} from "../images/gallery/index";
+import {gallery1} from "../images/gallery";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import NavMenu from "@/components/NavMenu";
