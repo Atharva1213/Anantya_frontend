e78bc46b76d580bde678a6ed4c3144dc86538df8
Tarun Rathod <tarunrathod00805@Taruns-MacBook-Air.local>
2024-03-12 20:17:27 +0530

    video cob=nverted in llink format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,11 @@ const Header = () => {
               opacity: 0.3,
             }}
           >
-            {/* <source src="/bgvideo1.mp4" type="video/mp4" /> */}
+            <source
+              src="https://res.cloudinary.com/drpj8yeqp/video/upload/v1710254576/Anantya_2024/bgvideo1_iggndi.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
           </video>
         </div>
         <div className="flex items-center justify-center">
@@ -51,15 +55,12 @@ const Header = () => {
               />
             </div>
           </div>
-          
         </div>
       </header>
-      <div style={{paddingBottom:"5vh"}}>
-      <CountdownTimer />
+      <div style={{ paddingBottom: "5vh" }}>
+        <CountdownTimer />
       </div>
     </>
-
-);
-
+  );
 };
 export default Header;
